d533f3fc675eece829ecf9710096fbec7ed1c24f
Douglas Lassance <douglassance@gmail.com>
2024-05-18 02:48:06 -0700

    Add initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+document.querySelectorAll(".scroll-nav a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth",
+        });
+    });
+});
